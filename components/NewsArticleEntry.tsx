5abe6281831e1c2f3bb23fe31ae066aefdd41664
unknown <manafikhiamjad@gmail.com>
2025-08-19 23:59:27 +0300

    change the image tag

--- components/NewsArticleEntry.tsx
+++ components/NewsArticleEntry.tsx
@@ -1,36 +1,35 @@
 import { NewsArticle } from "@/models/NewsArticles";
-import { useState } from "react";
-import Image from "next/image";
 import placeHolderImage from './assets/images/NewsPapers.jpg';
+
 type NewsArticleEntryProps = {
- article:NewsArticle
+    article: NewsArticle
 }
-export  const  NewsArticleEntry = ({article :{title, description, url, urlToImage}}:  NewsArticleEntryProps) => {
-    const [error, setError]=useState(false);
-    const validImage = urlToImage !== undefined ? urlToImage : undefined;
-    function fallbackImg(){
-        setError(true);
-    }
-    return ( 
-            <a href={url} >
-                <div className="rounded-md">
-                        <div className="w-100 rounded-md p-5 flex flex-col items-start justify-start">
-                            <Image 
-                                src={(!error&&validImage) ? validImage : placeHolderImage }
-                                alt={"news article image"}
-                                width={500}
-                                height={200}
-                                className="object-cover w-full rounded-t-lg"
-                                onError={() => fallbackImg()}
-                            />
-                            <div>
-                                <h1 className="text-2xl">{title}</h1>
-                                <p className="text-sm">{description}</p>
-                            </div>
-                        </div>
 
+export const NewsArticleEntry = ({ article: { title, description, url, urlToImage } }: NewsArticleEntryProps) => {
+    // Use a conditional to check for a valid image URL directly.
+    const imageSrc = urlToImage || placeHolderImage.src;
 
+    return (
+        <a href={url} target="_blank" rel="noopener noreferrer">
+            <div className="rounded-md">
+                <div className="w-100 rounded-md p-5 flex flex-col items-start justify-start">
+                    <img
+                        src={imageSrc}
+                        alt={"news article image"}
+                        width={500}
+                        height={200}
+                        className="object-cover w-full rounded-t-lg"
+                        onError={(e) => {
+                            // On error, directly set the src to the placeholder image
+                            e.currentTarget.src = placeHolderImage.src;
+                        }}
+                    />
+                    <div>
+                        <h1 className="text-2xl font-bold">{title}</h1>
+                        <p className="text-sm">{description}</p>
+                    </div>
                 </div>
-            </a>
+            </div>
+        </a>
     );
 }
